cb5dfaf51f6a6c63c3e04a8a191836342cc53741
Lance Hancock <lancehancockac1@gmail.com>
2022-07-28 10:17:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//While loop - repeat some code while some condition is true
+Console.Write("Enter your name!");
+String name = Console.ReadLine();
+while(name == "")
+{
+    Console.Write("Enter your name!");
+    name = Console.ReadLine();
+}
+Console.WriteLine("Hello "+name);
+
+Console.ReadKey();
